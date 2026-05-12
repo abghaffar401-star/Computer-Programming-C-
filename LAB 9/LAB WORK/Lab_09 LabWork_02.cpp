1457bab77d0435fa54ad54eb82aec27c1a5b270f
Abdul Ghafarr <abghaffar401@gmail.com>
2026-05-13 01:55:58 +0500

    Add files via upload

---
+++ LAB 9/LAB WORK/Lab_09 LabWork_02.cpp
@@ -0,0 +1,22 @@
+// Name: Abdul Ghafar
+// Department: Electrical Engineering (Power)
+// Registration No: BF25NWELE0711
+// Section: A
+
+#include <iostream>
+using namespace std;
+
+// Function declaration
+void sayHello()
+{
+    // Display message
+    cout << "Hello, World!" << endl;
+}
+
+int main()
+{
+    // Function call
+    sayHello();
+
+    return 0;
+}
